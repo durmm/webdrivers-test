18206ff5e38ee5f24bde54462bfc0ca98e97bb5a
durmm <gaya.stdev@gmail.com>
2018-01-16 12:07:41 +0400

    test class

--- src/test/java/TestWebdriver.java
+++ src/test/java/TestWebdriver.java
@@ -1,14 +1,16 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.opera.OperaOptions;
+import org.testng.annotations.Test;
 
 /**
  * Created by durmm on 16/01/18.
  */
-public class Test {
+public class TestWebdriver {
 
-    public WebDriver getLinuxDriver(char browser) {
+    private WebDriver getLinuxDriver(char browser) {
         WebDriver driver;
         switch (browser) {
             case 'F':
@@ -20,9 +22,21 @@ public class Test {
             case 'O':
                 System.setProperty("webdriver.opera.driver", "/var/www/webdrivers-test/linux drivers/operadriver64linux");
                 OperaOptions options = new OperaOptions();
-                options.setBinary("/");
+                options.setBinary("/usr/bin/opera");
+                return driver = new OperaDriver(options);
             default:
-
+                System.setProperty("webdriver.gecko.driver", "/var/www/webdrivers-test/linux drivers/geckodriver64linux");
+                return driver = new FirefoxDriver();
         }
+    }
+
+    @Test
+    public void linuxTest() {
+
+    }
+
+    @Test
+    public void windowsTest() {
+
     }
 }
